37941cc2055784b78762c2ececbd78c460e08a12
Aidan Fewster <fewstera@gmail.com>
2013-05-25 00:14:10 +0100

    Retrieval of definition, add template support (html/template.html), randomised CSS to prevent confliction with other sites.

--- src/common/content.js
+++ src/common/content.js
@@ -5,23 +5,35 @@
 // @require			jquery.min.js
 // ==/UserScript==
 
-var API_URL = 'http://localhost/definition';
+var $ = window.$.noConflict(true); // Required for Opera and IE
 
+var API_URL = 'http://definition.org/plugin/';
 
-//This is called by the onclick of the menu item
-//The contect menu is populated in *browser*/loadMenu.js
+var cssInjected = false;
+/*
+	*	Called by all the adapters when the definition button is clicked
+	*	begins displaying the content.	
+*/
 function loadDefinitions(defineText){
-
-	injectCSS();
-
+	var addonBox = document.getElementById('addonDefinitionXYZ');
+	if(addonBox!=null) 	
+		addonBox.parentNode.removeChild(addonBox); 
+	var overlayBox = document.getElementById('definitionOverlayAAAFxYts'); 
+	if(overlayBox!=null)
+		overlayBox.parentNode.removeChild(overlayBox);
+	if(!cssInjected)
+		injectCSS();
 	if(defineText.trim().split(' ').length<4){
-		alert('Loading your definiton of ' + defineText + ' right now! Nigger:' + defineText.trim().split(' ').length);
+		injectDefinitionHTML(defineText);
 	}else{
 		//Too many words selected
 	}
 }
 
 
+/*
+	*	Loads the two CSS files for the plugin into the page	
+*/
 function injectCSS(){
 	var details = {
         url: 'css/definePlugin.css',
@@ -31,7 +43,6 @@ function injectCSS(){
 	};
 	kango.xhr.send(details, function(data) {
 	        var content = data.response;
-	        kango.console.log(content);
 	        addStyle(content);
 	});
 	
@@ -43,10 +54,11 @@ function injectCSS(){
 	};
 	kango.xhr.send(details, function(data) {
 	        var content = data.response;
-		    kango.console.log(content);
 	        addStyle(content);
 	});
+	cssInjected = true;
 }
+
 
 function addStyle(cssCode, id) {
     if (id && document.getElementById(id))
@@ -77,3 +89,109 @@ function addStyle(cssCode, id) {
     if (father!=null)
         father.appendChild(styleElement);
 }
+
+
+function injectDefinitionHTML(defineText){
+	var details = {
+        url: 'html/template.html',
+        method: 'GET',
+        async: true,
+        contentType: 'text'
+	};
+	kango.xhr.send(details, function(data) {
+	        var content = data.response;
+	        var xPos, yPos;
+	        if(window.getSelection().type!="None"){
+		        var range = window.getSelection().getRangeAt(0);
+				range.collapse(false);
+				var dummy = document.createElement("span");
+				range.insertNode(dummy);
+				var rect = dummy.getBoundingClientRect();
+				xPos = rect.left;
+				yPos = rect.top+window.pageYOffset;
+				content = content.replace('{POSITIONING}', '');
+				content = content.replace('{ARROW-STYLE}', '');
+			}else{
+				content = content.replace('{POSITIONING}', 'position: fixed !important;');
+				content = content.replace('{ARROW-STYLE}', ' style="display:none;"');
+				xPos = 450;
+				yPos = 150;
+			}
+			//dummy.parentNode.removeChild(dummy);
+	        content = content.replace('{X-POSITION}', xPos-30);
+	        content = content.replace('{Y-POSITION}', yPos-38);
+	        
+	        var defineReq = {
+		        url: 'http://definition.org/definitionJSON.php?word=' + defineText,
+		        method: 'GET',
+		        async: true,
+		        contentType: 'text'
+		    };
+		    kango.xhr.send(defineReq, function(defineJSON) {
+		    	defineJSON = JSON.parse(defineJSON.response);
+		    	console.log(defineJSON);
+		    	
+		    	content = content.replaceAll('{WORD}', defineText);
+		    	content = content.replaceAll('{HYPH-WORD}', defineJSON.hyphenatedWord);
+		    	
+		    	var partsOfSpeach = "";
+		    	for (var i = 0; i < defineJSON.partsOfSpeach.length; i++) {
+		    		partsOfSpeach = partsOfSpeach + "" + defineJSON.partsOfSpeach[i] + ", "; 
+		    	}
+		    	
+		    	partsOfSpeach = partsOfSpeach.substring(0, partsOfSpeach.length - 2);
+		    	content = content.replace('{PARTS-SPEACH}', partsOfSpeach);
+
+		    	
+		    	var definitions = "";
+		    	for (var i = 0; i < defineJSON.definitions.length; i++) {
+		    		definitions = definitions + "\n<li>" + defineJSON.definitions[i] + "</li>"; 
+		    	}	
+
+		    	content = content.replace('{DEFINITIONS}', definitions);
+
+
+		    	var synonyms = "";
+		    	for (var i = 0; i < defineJSON.synonyms.length; i++) {
+		    		synonyms = synonyms + "\n<li>" + defineJSON.synonyms[i] + ", </li>"; 
+		    	}			    	
+		    	
+		    	if(synonyms == "")
+		    		synonyms = "<li>Sorry no synonyms have been found for " + defineText + ".</li>";
+		    	
+		    	content = content.replace('{SYNONYMS}', synonyms);
+
+	        	$("body").append(content);
+	        });
+	});
+}
+
+
+String.prototype.replaceAll = function( token, newToken, ignoreCase ) {
+    var _token;
+    var str = this + "";
+    var i = -1;
+
+    if ( typeof token === "string" ) {
+
+        if ( ignoreCase ) {
+
+            _token = token.toLowerCase();
+
+            while( (
+                i = str.toLowerCase().indexOf(
+                    token, i >= 0 ? i + newToken.length : 0
+                ) ) !== -1
+            ) {
+                str = str.substring( 0, i ) +
+                    newToken +
+                    str.substring( i + token.length );
+            }
+
+        } else {
+            return this.split( token ).join( newToken );
+        }
+
+    }
+return str;
+};
